7acddd54fea7cc23b047790c2d2ed410fdd79c8d
Wordluc <40872910+Wordluc@users.noreply.github.com>
2024-03-09 12:14:50 +0100

    refresh switch tra file nvim

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,32 +1,32 @@
-local lsp=require('lsp-zero')
+local lsp = require('lsp-zero')
 lsp.preset("recommended")
-
+local builtin = require('telescope.builtin')
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "gl", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "gh", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>g", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "gr", function() vim.lsp.buf.rename() end, opts)
- -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-  vim.keymap.set("n", "<C-g>", function() vim.diagnostic.open_float() end, opts)
+	local opts = { buffer = bufnr, remap = false }
+	lsp.buffer_autoformat()
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "gl", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "gh", function() vim.diagnostic.goto_prev() end, opts)
+	vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>g", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "gr", function() vim.lsp.buf.rename() end, opts)
+	-- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "<C-g>", function() builtin.diagnostics({ severity_limit = 1 }) end, opts)
 end)
-
+vim.api.nvim_create_autocmd({ "BufEnter" }, { command = ":e" })
 lsp.setup({
-  virtual_text = true,
-  signs = true,
-  update_in_insert = true,
-  underline = true,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = 'minimal',
-    border = 'rounded',
-    source = 'always',
-    header = '',
-    prefix = '',
-  },
+	virtual_text = true,
+	signs = true,
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = 'minimal',
+		border = 'rounded',
+		source = 'always',
+		header = '',
+		prefix = '',
+	},
 })
