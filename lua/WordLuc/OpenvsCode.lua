d9f1aac497825acf9c8706471bdc4d8c602d8bea
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2024-05-08 16:10:31 +0200

    fix merge

--- lua/WordLuc/OpenvsCode.lua
+++ lua/WordLuc/OpenvsCode.lua
@@ -1,9 +1,12 @@
 local function openVs(path, nLess)
 	local segments = {}
+<<<<<<< HEAD
 	if path == '""' or nLess == -1 then
 		vim.cmd("! code -r .")
 		return;
 	end
+=======
+>>>>>>> parent of 51dd442 (feat: fix open vscode)
 	for segment in path:gmatch("[^/|\\]+") do
 		table.insert(segments, segment)
 	end
@@ -13,14 +16,25 @@ end
 
 vim.keymap.set("n", ":vs", function()
 	local path = vim.fn.shellescape(vim.fn.expand("%:p"))
+	if path == '""' then
+		vim.cmd("! code -r .")
+		return;
+	end
 	openVs(path, 1)
 end)
 
 vim.keymap.set("n", ":vs-", function()
 	local path = vim.fn.shellescape(vim.fn.expand("%:p"))
+	if path == '""' then
+		vim.cmd("! code -r .")
+		return;
+	end
 	openVs(path, 2)
 end)
 
+<<<<<<< HEAD
 vim.keymap.set("n", ":vsr", function()
 	openVs(nil, -1)
 end)
+=======
+>>>>>>> parent of 51dd442 (feat: fix open vscode)
