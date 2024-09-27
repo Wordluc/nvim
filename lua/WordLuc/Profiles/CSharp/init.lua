803e25e9e3fc6a0bc31d8186f30f0962b1d98682
Luca Frangiamore <Luca.Frangiamore@worlwolterskluwer.com>
2024-09-27 17:39:23 +0200

    feat: clean

--- lua/WordLuc/Profiles/CSharp/init.lua
+++ lua/WordLuc/Profiles/CSharp/init.lua
@@ -1,5 +1,6 @@
 print("Welcome to C#")
 local uv = require('luv')
+local util_roslyn = require("roslyn.generalUtils")
 --Default_setup("csharp_ls")
 require("roslyn").setup({
 	choose_sln = function(strs)
@@ -90,84 +91,9 @@ require("roslyn").setup({
 		}
 	}
 })
-local addElement = function(path)
-	uv.run("nowait") -- This is necessary to start the event loop
-	local csprojPath = ""
-	local nameCsproj = ""
-	local sameLevelElement = ""
-	local nameFile = path:match("([^/\\]+)$")
-	for entry, type in vim.fs.dir(path:sub(1, - #nameFile - 1)) do
-		if type == "file" then
-			if entry ~= nameFile then
-				sameLevelElement = entry
-				break
-			end
-		end
-	end
-	--sameLevelElement = vim.fs.joinpath(path, sameLevelElement)
-
-	vim.fs.find(function(name, _path)
-		if name:match("%.csproj$") then
-			csprojPath = _path
-			nameCsproj = name
-			return true
-		end
-		return false
-	end, { upward = true, type = "file", limit = math.huge, path = path })
-	local pathRelativeFile = ""
-	if pathRelativeFile == nil then
-		print("Error adding element")
-		return
-	end
-	pathRelativeFile = path:sub(#csprojPath + 2)
-	local nameRelativeFile = pathRelativeFile:match("([^/\\]+)$")
-	pathRelativeFile = pathRelativeFile:gsub("/", "\\")
-	sameLevelElement = pathRelativeFile:sub(1, - #nameRelativeFile - 2) .. "\\" .. sameLevelElement
-	sameLevelElement = sameLevelElement:gsub("/", "\\")
-	local stdin = uv.new_pipe()
-	local stdout = uv.new_pipe()
-	local stderr = uv.new_pipe()
-	--local script_path = debug.getinfo(1, "S").source:sub(2)
-	--local executable_path = script_path:match(".*/") .. "path from package lua"
-	local executable_path = "C:\\Users\\Usernexus\\Desktop\\PersonalProject\\AddElementToCsproj\\AddElementToCsproj.exe"
-	local handle, pid = uv.spawn(
-		executable_path
-		, { stdio = { stdin, stdout, stderr } }, function()
-			stdin:close()
-			stdout:close()
-			stderr:close()
-		end
-	)
-	if not pid or not handle then
-		print("Error adding element")
-		return
-	end
-	local input = {
-		CsprojPath = vim.fs.joinpath(csprojPath, nameCsproj),
-		WhenAddElement = sameLevelElement,
-		ElementToAdd = string.format('<Compile Include="%s" />', pathRelativeFile)
-	}
-	local json = vim.fn.json_encode(input)
-	stdin:write(json .. "\n")
-	stdout:read_start(function(e, data)
-		assert(not e, e)
-		if data then
-			vim.schedule(function()
-				print(data)
-				require("roslyn.slnutils").did_change_watched_file(vim.uri_from_bufnr(0))
-			end)
-		end
-	end)
-	stderr:read_start(function(e, data)
-		assert(not e, e)
-		if data then
-			print(data)
-		end
-	end)
-end
 
 vim.api.nvim_create_user_command('AddedFile', function() --da attaccare a treeneovim
-	addElement(vim.fn.expand("%:p"))
+	require("roslyn.csprojManager").addElement(vim.fn.expand("%:p"))
 end, { bang = true, nargs = '*' })
 
 vim.api.nvim_create_user_command('Gnamespace', function()
