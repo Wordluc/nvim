d952549e8b560077b50e166ff3a844d5dd6c7ca6
Luca Frangiamore <Luca.Frangiamore@worlwolterskluwer.com>
2024-12-12 09:15:50 +0100

    feat: optimization telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,18 @@
 return {
 	"Wordluc/telescope.nvim",
-	dependencies = "tsakirist/telescope-lazy.nvim",
+	dependencies = {"tsakirist/telescope-lazy.nvim","nvim-telescope/telescope-fzy-native.nvim"},
 	config = function()
 		local extra = {}
 		if EnvManage.isEnv(EnvEnum.wki) then
 			extra = { "%.d.ts", "%.js", "%.css", "%.dgml", "%.csso", "%.csso.map", "%.jso.d.ts.map", "%.jso.map" }
 		end
 		require("telescope").setup {
+			extensions = {
+				fzy_native = {
+					override_generic_sorter = false,
+					override_file_sorter = true,
+				}
+			},
 			defaults = {
 				path_display = {
 					filename_first = {
@@ -36,29 +42,31 @@ return {
 		}
 		require("telescope").load_extension("lazy")
 		require("telescope").load_extension("marks_nvim")
+		require('telescope').load_extension('fzy_native')
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Find files"})
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
 		vim.keymap.set('n', '<leader>fvv', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end, {desc = "Grep text"})
+		end, { desc = "Grep text" })
 		vim.keymap.set('v', '<leader>fvv', function()
 			vim.cmd('noau normal! "ay"')
 			builtin.grep_string({ search = vim.fn.getreg("a") })
-		end, {desc = "Grep text selected"})
+		end, { desc = "Grep text selected" })
 		vim.keymap.set('n', '<leader>fb', function()
 			builtin.buffers({ sort_mru = true })
-		end, {desc = "Find buffers"})
+		end, { desc = "Find buffers" })
 
 		vim.keymap.set('n', '<leader>fm', function()
 			require('telescope').extensions.marks_nvim.marks_list_all() --[[ List all marks ]]
-		end, {desc = "Find buffers"})
+		end, { desc = "Find buffers" })
 
 		vim.keymap.set('n', '<leader>fh', function()
 			builtin.lsp_references()
-		end, {desc = "Find LSP references"})
-		vim.keymap.set('n', '<leader>fv', builtin.live_grep, {desc = "Live Grep"})
-		vim.keymap.set('n', '<leader>fg', builtin.git_status, {desc = "Git status"})
-		vim.keymap.set('n', '<leader>fl', function()builtin.builtin({filter_match="lsp_",title="LSP Picker"}) end,{desc="LSP Functions"})
-		vim.keymap.set('n', '<C-x>',function() builtin.keymaps() end,{desc="Keymaps"})
+		end, { desc = "Find LSP references" })
+		vim.keymap.set('n', '<leader>fv', builtin.live_grep, { desc = "Live Grep" })
+		vim.keymap.set('n', '<leader>fg', builtin.git_status, { desc = "Git status" })
+		vim.keymap.set('n', '<leader>fl', function() builtin.builtin({ filter_match = "lsp_", title = "LSP Picker" }) end,
+		{ desc = "LSP Functions" })
+		vim.keymap.set('n', '<C-x>', function() builtin.keymaps() end, { desc = "Keymaps" })
 	end
 }
